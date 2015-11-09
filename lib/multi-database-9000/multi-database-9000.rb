80a67afd47b1cd436c00ceaea5944fb984b34ff7
Mark Weston <mark@markweston.me.uk>
2015-11-09 17:03:52 +0000

    removed some pointless code duplication - methods used by rake tasks still to be refactored

--- lib/multi-database-9000/multi-database-9000.rb
+++ lib/multi-database-9000/multi-database-9000.rb
@@ -14,42 +14,4 @@ module MultiDatabase9000
     return nil unless database.present?
     return ["db/#{database}_migrate"]
   end
-
-  def database_connections(database: nil, rails_envs: nil)
-    connections = connections_for_environment(rails_envs)
-    if database.present?
-      connections.keep_if {|key, _| key.match Regexp.new(database)}
-    end
-    return connections
-  end
-
-  def connections_for_environment(rails_envs)
-    rails_envs = Array(rails_envs)
-    matcher = ->(key, value){rails_envs.any?{|env| key.match Regexp.new(env)}}
-    return ActiveRecord::Base.configurations.keep_if &matcher
-  end
-
-  def database_name(connection_key)
-    if connection_key.match /\w+_\w+/
-      return connection_key.split('_').first
-    else
-      return "default"
-    end
-  end
-
-  def migration_directory(connection_key)
-    if ['test','development','staging','cucumber','production'].include? connection_key
-      return "db/migrate/"
-    else
-      return "db/#{database_name(connection_key)}_migrate/"
-    end
-  end
-
-  def schema_file_name(connection_key)
-    if ['test','development','staging','cucumber','production'].include? connection_key
-      return "schema.rb"
-    else
-      return "#{database_name(connection_key)}_schema.rb"
-    end
-  end
 end
